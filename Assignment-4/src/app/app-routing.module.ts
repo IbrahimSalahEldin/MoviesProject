4aa289d16c135d6205b599021410f4cad098f876
Ibrahim Salah <57736764+IbrahimSalahEldin@users.noreply.github.com>
2023-08-05 23:21:23 -1000

    add routing

--- Assignment-4/src/app/app-routing.module.ts
+++ Assignment-4/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { MoviesListComponent } from './content/movies-list/movies-list.component';
+import { RoomsComponent } from './content/rooms/rooms.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: ContentComponent},
+  {path:'content/movies-list',component: MoviesListComponent},
+  {path:'content/rooms',component: RoomsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Assignment-4/src/app/app.module.ts
+++ Assignment-4/src/app/app.module.ts
@@ -6,13 +6,16 @@ import { FooterComponent } from './footer/footer.component';
 import { DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ContentComponent } from './content/content.component';
-
+import { MoviesListComponent } from './content/movies-list/movies-list.component';
+import { RoomsComponent } from './content/rooms/rooms.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   
-  declarations: [AppComponent, HeaderComponent, FooterComponent, ContentComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, ContentComponent, MoviesListComponent, RoomsComponent],
+  imports: [BrowserModule, FormsModule,  RouterModule],
   providers: [DatePipe],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule {}
