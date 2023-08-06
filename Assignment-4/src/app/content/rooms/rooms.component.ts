cecc63e9f246f1c6c3483a9b6bfb648d4f87b985
amiramakram <amiramakram990@gmail.com>
2023-08-06 03:58:38 +0200

    update

--- Assignment-4/src/app/content/rooms/rooms.component.ts
+++ Assignment-4/src/app/content/rooms/rooms.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent {
 
+export class RoomsComponent {
+  movies: any;
+  data:any;
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http.get('assets/movies.json').subscribe((data: any) => {
+
+      this.movies= data;
+      // console.log(data); 
+    });
+  }
+
+  Room( room1:number){
+    
+  const newArr=[...this.movies];
+  this.RandomMovies(newArr);
+  const res=newArr.slice(0,room1);
+  this.data=res;
+  console.log(this.data);
+  }
+  RandomMovies(moviesData:any[]){
+    for(let i=moviesData.length;i>0;i--) {
+    const roomsRandom =Math.floor(Math.random()*(i+1));
+[moviesData[i],moviesData[roomsRandom]]=[moviesData[roomsRandom],moviesData[i]]
+    }
+  }
 }
